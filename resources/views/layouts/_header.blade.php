57120603c6ca5a7828ef6f3ba3742f3b4f3b81b2
liuguowei <liuguowei163@yeah.net>
2019-12-21 14:40:05 +0000

    切割头部和尾部子视图

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,15 @@
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <div class="container ">
+    <a class="navbar-brand" href="#">Weibo</a>
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">帮助</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">登录</a>
+      </li>
+    </ul>
+  </div>
+
+</nav>
