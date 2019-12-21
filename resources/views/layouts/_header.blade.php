937330b4289147db219376870ad7edb9dc624f80
liuguowei <liuguowei163@yeah.net>
2019-12-21 14:49:51 +0000

    命名路由

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,10 +1,10 @@
 
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
   <div class="container ">
-    <a class="navbar-brand" href="#">Weibo</a>
+    <a class="navbar-brand" href="{{ route('home') }}">Weibo</a>
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" href="#">帮助</a>
+        <a class="nav-link" href="{{ route('help') }}">帮助</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">登录</a>
