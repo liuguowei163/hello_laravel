dbd2680dc99c20cb8087a362ba88fd3f86ff793b
liuguowei <liuguowei163@yeah.net>
2019-12-23 02:32:46 +0000

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,7 +12,17 @@ class UsersController extends Controller
         return view('users.create');
     }
 
+    //展示个人页面
     public function show(User $user){
         return view('users.show', compact('user'));
+    }
+    //注册页面的提交验证方法
+    public function store(Request $request){
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:50',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
     }
 }
