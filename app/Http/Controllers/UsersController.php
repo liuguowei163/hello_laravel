a4332112bf61b7aea989356f044f4d619d54e86a
liuguowei <liuguowei163@yeah.net>
2019-12-27 13:39:46 +0000

    关注和粉丝列表页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -109,4 +109,16 @@ class UsersController extends Controller
         session()->flash('success', '成功删除用户');
         return back();
     }
+    //显示用户关注人的列表
+    public function followings(User $user){
+        $users = $user->followings()->paginate(30);
+        $title = $user->name . '关注的人';
+        return view('users.show_follow', compact('users', 'title'));
+    }
+    //显示用户的粉丝
+    public function followers(User $user){
+        $users = $user->followers()->paginate(30);
+        $title = $user->name . '的粉丝';
+        return view('users.show_follow', compact('users', 'title'));
+    }
 }
